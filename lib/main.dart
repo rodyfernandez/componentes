7b82e77b12bea2b3eabec1dfe744bbd0cf5615e7
Rodolfo Fernandez <rodyfernandez87@gmail.com>
2019-05-20 23:12:31 -0300

    seccion componentes 6 , capitulo futurebuilder 67

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:componentes/src/pages/home_page.dart';
+//import 'package:componentes/src/pages/home_temp.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
+      
+    );
+  }
+}
